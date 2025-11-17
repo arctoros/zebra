cda687b8295a623b867624a41d9b7920962df0f5
Arctoros <95149413+arctoros@users.noreply.github.com>
2025-11-17 10:54:44 +0000

    Delete bios directory

--- bios/misc/helloworld.asm
+++
@@ -1,8 +0,0 @@
-message:db $0D, $0A, '!', ' ', 'd', 'l', 'r', 'o', 'w', ' ', 'o', 'l', 'l', 'e', 'H'
-
-				ld hl, message + 14
-hello:	ld a, (hl)
-				call printChar
-				dec l
-				jp nz, hello
-				jp loop
